0fea9dc9e44d1966f370e1d008a0c73df3a9b155
Pradeep Kumar <pradeep.ugrasen@gmail.com>
2025-03-02 21:37:15 +0530

    firstCommit

---
+++ data-structures-and-algorithms/src/com/cubicoder/math/CountDigit.java
@@ -0,0 +1,25 @@
+package com.cubicoder.math;
+
+import java.util.Scanner;
+
+public class CountDigit {
+
+	public static int digitCount(int num) {
+		int count = 0;
+		while (num > 0) {
+			num = num / 10;
+			count++;
+		}
+
+		return count;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+
+		System.out.println("No Of Digit :: " + CountDigit.digitCount(num));
+		sc.close();
+	}
+}
